3dbb5e91e32aeaf26788b69647b04a85f251974f
Michelle <mweng@buffalo.edu>
2022-06-19 12:18:49 -0400

    certbott

--- nginx/certbot-first.sh
+++ nginx/certbot-first.sh
@@ -1,5 +1,5 @@
 docker run -it --rm --name certbot \
-            -p 242:80 -p 443:443 \
+            -p 80:80 -p 443:443 \
             -v "/srv/nginx/cert:/etc/letsencrypt" \
             certbot/certbot certonly --standalone -d www.encoderlist.click -d encoderlist.click \
                  --non-interactive --agree-tos \
